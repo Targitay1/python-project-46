14d6876d6654b44176190d60aae2e1abc567eebf
Targitay1 <cibrenok@bk.ru>
2025-11-06 00:56:48 +0300

    Add working tests

--- tests/test_brain_games.py
+++ tests/test_brain_games.py
@@ -1,4 +1,4 @@
-"""Basic tests for Brain Games."""
+"""Basic tests for Brain Games package."""
 def test_import():
     """Test that package can be imported."""
     try:
@@ -6,3 +6,11 @@ def test_import():
         assert True
     except ImportError:
         assert False, "Cannot import brain_games"
+
+def test_cli_import():
+    """Test that CLI module can be imported."""
+    try:
+        from brain_games.cli import welcome_user
+        assert True
+    except ImportError:
+        assert False, "Cannot import welcome_user"
